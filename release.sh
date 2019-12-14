40d012d2fd06970ec9055d80f1eea7247136a363
htpc-helper <htpc-helper@gmail.com>
2019-12-14 23:35:36 +0800

    fix: release 1

--- release.sh
+++ release.sh
@@ -11,14 +11,14 @@ echo "Downloading k3os version: v${RELEASE_VERSION}"
 curl -Lo k3os-amd64.iso $RELEASE_URL
 
 # Edit the iso
-mount -o loop k3os.iso /mnt
+sudo mount -o loop k3os.iso /mnt
 mkdir -p iso/boot/grub
 cp -rf /mnt/k3os iso/
 rm iso/system/config.yaml
 cp config.yaml iso/system/
 cp /mnt/boot/grub/grub.cfg iso/boot/grub/
 grub-mkrescue -o k3os-${RELEASE_VERSION}.iso iso/ 
-umount /mnt
+sudo umount /mnt
 
 # Transfer iso to s3
 # mc config host add s3 $ENDPOINT $ACCESS_KEY $SECRET_KEY
